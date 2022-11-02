931430423d3f871979cf2e37d24488bc8f444f1a
Visakh Vijayan <visakh.vijayan@seenit.in>
2022-11-03 01:07:20 +0530

    fixed initialy accuracy showing up as Nan

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { Phrase } from './components/Phrase';
 import { quotes } from './config/constants';
@@ -12,19 +13,25 @@ function App() {
   useEffect(() => {
     const randomNumber = Math.floor(Math.random() * quotes.length);
 
-    console.log(quotes[randomNumber].text);
     setLorem(quotes[randomNumber].text + quotes[randomNumber + 1].text + quotes[randomNumber + 2].text);
     setIsLoading(false);
 
   }, []);
 
-  const typeHandler = (e) => {
-
-    setTypedContent(e.target.value);
+  useEffect(() => {
     updateAccuracy();
+  }, [typedContent.length])
+
+  const typeHandler = (e) => {
+    setTypedContent(e.target.value);
   }
 
   const updateAccuracy = () => {
+
+    if (isNaN(typedContent.length) || !typedContent.length) {
+      return;
+    }
+
     let correct = 0;
 
     for (let i = 0; i < typedContent.length; i++) {
@@ -34,6 +41,7 @@ function App() {
     }
 
     const ratio = parseInt(correct/typedContent.length * 100);
+
     setAccuracy(ratio);
   }
 
@@ -44,7 +52,7 @@ function App() {
         <p className='subLabel'>A programmer's life ...</p>
       </div>
       {isLoading ? <div className='content'>Please wait</div> : <div className='content'>
-        <h4 className='label'>Accuracy: {accuracy}%</h4>
+        <h4 className='label'>Accuracy: {accuracy}% | CPM: 0</h4>
         <Phrase original={lorem} typed={typedContent}/>
         <input className='inputBox' onKeyUp={typeHandler} autoFocus={true} onBlur={({ target }) => target.focus()} />
       </div>}
